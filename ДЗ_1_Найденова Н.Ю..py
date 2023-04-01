6b0fd3f7ac4f6641e3a7a5335edad3ba49ab6ab8
Natusy71 <111445091+Natusy71@users.noreply.github.com>
2023-04-01 11:53:14 +0500

    ДЗ_1_ Найденова Н.Ю.
    На проверку

---
+++ ДЗ_1_Найденова Н.Ю..py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+for _ in range(10):
+    print("Найденова Наталья Юрьевна")
+
+
+# In[7]:
+
+
+kd=float(input("Введите курс евро"))
+dollar=float(input("Введите количество долларов"))
+print("Перевести из долларов в евро\n")
+a1=dollar/kd
+print("Сумма вашего перевода из доллара в евро = ",'{:.2f}'.format(a1))
+
+
+# In[ ]:
+
+
+
+
